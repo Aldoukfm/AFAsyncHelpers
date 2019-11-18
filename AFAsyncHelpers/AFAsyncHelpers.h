59a6f75df36776ef54f67edb4527d47cb34d17ba
Aldo Fuentes <35090917+Aldoukfm@users.noreply.github.com>
2019-11-17 22:17:55 -0600

    Initial Commit

---
+++ AFAsyncHelpers/AFAsyncHelpers.h
@@ -0,0 +1,19 @@
+//
+//  AFAsyncHelpers.h
+//  AFAsyncHelpers
+//
+//  Created by Aldo Fuentes on 17/11/19.
+//  Copyright © 2019 Aldo Fuentes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AFAsyncHelpers.
+FOUNDATION_EXPORT double AFAsyncHelpersVersionNumber;
+
+//! Project version string for AFAsyncHelpers.
+FOUNDATION_EXPORT const unsigned char AFAsyncHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFAsyncHelpers/PublicHeader.h>
+
+
